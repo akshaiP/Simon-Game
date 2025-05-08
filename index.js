7eeaa53ac13a0ad68e919a2a2814f1a56bf9412c
akshaiP <158182355+akshaiP@users.noreply.github.com>
2025-05-08 20:29:39 +0530

    audio fix

--- index.js
+++ index.js
@@ -76,23 +76,23 @@ function playSound(color){
     switch(color)
     {
         case "blue":
-            var blue = new Audio("/sounds/blue.mp3");
+            var blue = new Audio("sounds/blue.mp3");
             blue.play();
             break;
         case "green":
-            var green = new Audio("/sounds/green.mp3");
+            var green = new Audio("sounds/green.mp3");
             green.play();
             break; 
         case "red":
-            var red = new Audio("/sounds/red.mp3");
+            var red = new Audio("sounds/red.mp3");
             red.play();
             break; 
         case "yellow":
-            var yellow = new Audio("/sounds/yellow.mp3");
+            var yellow = new Audio("sounds/yellow.mp3");
             yellow.play();
             break; 
         case "wrong":
-            var wrong = new Audio("/sounds/wrong.mp3");
+            var wrong = new Audio("sounds/wrong.mp3");
             wrong.play();
             break;  
         default : console.error();
